f79ea77ddedf627590b3a3fd551f27c4d97a9363
izzwerg <izzwerg.kr@gmail.com>
2024-01-14 23:21:52 +0200

    ico

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -36,7 +36,7 @@ let choosenDate = "";
 function dateValidate() {
     if (choosenDate.getTime() < Date.now()) {
         iziToast.show({
-            iconUrl: '../img/error.svg',
+            iconUrl: '../public/error.svg',
             iconColor: 'white',
             color: '#EF4040',
             message: 'Please choose a date in the future'
